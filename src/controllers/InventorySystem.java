3b7d35ffaa0e584970f10bd9b894f5e89e58838c
Ayannrwz <enriquezadrian.aws@gmail.com>
2023-09-21 13:43:33 +0800

    Created an InventorySystem.java

--- src/controllers/InventorySystem.java
+++ src/controllers/InventorySystem.java
@@ -1,5 +1,24 @@
 package controllers;
 
-public class InventorySystem {
+import java.util.ArrayList;
 
+import models.Drinks;
+import models.Foods;
+
+public class InventorySystem {
+	private ArrayList<Drinks> drinksList = new ArrayList<>();
+	private ArrayList<Foods> foodsList = new ArrayList<>();
+	private ArrayList<Transaction> transactions = new ArrayList<>();
+
+	public void addDrinks(Drinks drinks) {
+		drinksList.add(drinks);
+	}
+
+	public void addFoods(Foods foods) {
+		foodsList.add(foods);
+	}
+
+	public void addTransaction(Transaction transaction) {
+		transactions.add(transaction);
+	}
 }
