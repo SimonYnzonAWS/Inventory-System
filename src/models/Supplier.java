8ae62de108c635f50141aafd26f41d04f1876337
AcamarBaltazar <baltazaracamar.aws@gmail.com>
2023-09-25 16:23:09 +0800

    Updated Supplier.java

--- src/models/Supplier.java
+++ src/models/Supplier.java
@@ -1,1 +1,25 @@
+package models;
 
+public class Supplier {
+	private String supplierName;
+	private int supplierID;
+	
+	public String getSupplierName() {
+		return supplierName;
+	}
+	public void setSupplierName(String supplierName) {
+		this.supplierName = supplierName;
+	}
+	public int getSupplierID() {
+		return supplierID;
+	}
+	public void setSupplierID(int supplierID) {
+		this.supplierID = supplierID;
+	}
+	
+	public String toString() {
+		return "Supplier: " + supplierName
+				+ "\n Supplier ID: " +supplierID;
+	}
+	
+}
